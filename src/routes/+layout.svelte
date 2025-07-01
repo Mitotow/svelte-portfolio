634b63676c6a0b3971d25ac5150a306e80235344
E224340Y <thomas.ambroise@etu.univ-nantes.fr>
2025-07-01 11:39:40 +0200

    refactor: remove one header folder from lib

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
     import "../app.css";
-    import Header from "$lib/components/header/header/Header.svelte";
+    import Header from "$lib/components/header/Header.svelte";
 </script>
 
 <Header />
